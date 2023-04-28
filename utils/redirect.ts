978fe786e26c398edd32131e7c49b00a7b436fac
andrewdelanoy <andrewdelanoy@hotmail.com>
2023-04-28 05:14:35 -0400

    Added copyright header to redirect.ts

--- utils/redirect.ts
+++ utils/redirect.ts
@@ -1,3 +1,4 @@
+// Copyright 2023 the Deno authors. All rights reserved. MIT license.
 export function RedirectHelper(localPath: string, statusCode: number) {
   return new Response("", {
     status: statusCode,
