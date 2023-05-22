2ec0c6e74a9a385aeaf9c89f7f25b4118a607625
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-22 11:25:29 +1000

    tweaks

--- components/UserPostedAt.tsx
+++ components/UserPostedAt.tsx
@@ -1,14 +1,6 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import type { User } from "@/utils/db.ts";
-import { pluralize } from "@/utils/display.ts";
-
-/** @todo Replace with https://deno.land/std@0.184.0/datetime/mod.ts?s=difference */
-function timeAgo(time: number | Date) {
-  const between = (Date.now() - Number(time)) / 1000;
-  if (between < 3600) return pluralize(~~(between / 60), "minute");
-  else if (between < 86400) return pluralize(~~(between / 3600), "hour");
-  else return pluralize(~~(between / 86400), "day");
-}
+import { timeAgo } from "@/utils/display.ts";
 
 export default function UserPostedAt(
   props: { user: User; createdAt: Date },
--- utils/display.ts
+++ utils/display.ts
@@ -2,3 +2,14 @@
 export function pluralize(unit: number, label: string) {
   return unit === 1 ? `${unit} ${label}` : `${unit} ${label}s`;
 }
+
+/**
+ * @todo Replace with https://deno.land/std@0.184.0/datetime/mod.ts?s=difference
+ * @todo Tests
+ */
+export function timeAgo(time: number | Date) {
+  const between = (Date.now() - Number(time)) / 1000;
+  if (between < 3600) return pluralize(~~(between / 60), "minute");
+  else if (between < 86400) return pluralize(~~(between / 3600), "hour");
+  else return pluralize(~~(between / 86400), "day");
+}
