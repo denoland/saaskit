887576de68fe7f4c5b6a7b503b3b7f7a2b03d171
brunocorrea23 <brunocorrea23@gmail.com>
2023-05-25 13:58:30 -0300

    pr review - changing layout of the user page

--- routes/user/[username].tsx
+++ routes/user/[username].tsx
@@ -2,10 +2,10 @@
 import type { Handlers, PageProps } from "$fresh/server.ts";
 import Head from "@/components/Head.tsx";
 import Layout from "@/components/Layout.tsx";
-import { BUTTON_STYLES, NOTICE_STYLES } from "@/utils/constants.ts";
 import { ComponentChild } from "preact";
 import { getUserByLogin, type User } from "@/utils/db.ts";
 import type { State } from "@/routes/_middleware.ts";
+import { SITE_WIDTH_STYLES } from "@/utils/constants.ts";
 
 export interface UserData extends State {
   user: User;
@@ -20,10 +20,7 @@ export const handler: Handlers<UserData, State> = {
       return ctx.renderNotFound();
     }
 
-    return ctx.render({
-      ...ctx.state,
-      user: user!,
-    });
+    return ctx.render({ ...ctx.state, user });
   },
 };
 
@@ -36,53 +33,42 @@ interface RowProps {
 
 function Row(props: RowProps) {
   return (
-    <li class="py-4">
-      <div class="flex flex-wrap justify-between">
-        <div>
-          <div class="flex flex-wrap justify-between">
-            <span>
-              <strong>{props.title}</strong>
-            </span>
-            {props.children && <span>{props.children}</span>}
-          </div>
-          <p>
-            {props.text}
-          </p>
+    <div class="flex flex-wrap">
+      {props.img && (
+        <img
+          height="48"
+          width="48"
+          src={props.img}
+          alt="user avatar"
+          class="rounded-full"
+        />
+      )}
+      <div class="px-4">
+        <div class="flex flex-wrap justify-between">
+          <span>
+            <strong>{props.title}</strong>
+          </span>
+          {props.children && <span>{props.children}</span>}
         </div>
-        {props.img && (
-          <img
-            height="48"
-            width="48"
-            src={props.img}
-            alt="user avatar"
-            class="rounded-full"
-          />
-        )}
+        <p>
+          {props.text}
+        </p>
       </div>
-    </li>
+    </div>
   );
 }
 
 export default function UserPage(props: PageProps<UserData>) {
   return (
     <>
-      <Head title="User" href={props.url.href} />
+      <Head title={props.data.user.login} href={props.url.href} />
       <Layout session={props.data.sessionId}>
-        <div class="max-w-lg m-auto w-full flex-1 p-4 flex flex-col justify-center">
-          <h1 class="text-3xl mb-4">
-            <strong>User</strong>
-          </h1>
-          <ul>
-            <Row
-              title="id"
-              text={props.data.user.id}
-            />
-            <Row
-              title="Username"
-              text={props.data.user.login}
-              img={props.data.user.avatarUrl}
-            />
-          </ul>
+        <div class={`${SITE_WIDTH_STYLES} flex-1 px-4`}>
+          <Row
+            title="Username"
+            text={props.data.user.login}
+            img={props.data.user.avatarUrl}
+          />
         </div>
       </Layout>
     </>
