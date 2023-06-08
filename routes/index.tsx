0381601fc46b15e07d7ddc33ec10e0c359f655a2
brunocorrea23 <brunocorrea23@gmail.com>
2023-06-08 20:44:50 -0300

    extract page selector

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,6 +5,7 @@ import Layout from "@/components/Layout.tsx";
 import Head from "@/components/Head.tsx";
 import type { State } from "./_middleware.ts";
 import ItemSummary from "@/components/ItemSummary.tsx";
+import PageSelector from "@/components/PageSelector.tsx";
 import {
   compareScore,
   getAllItemsInTimeAgo,
@@ -60,32 +61,6 @@ export const handler: Handlers<HomePageData, State> = {
     return ctx.render({ ...ctx.state, items, itemsUsers, areVoted, lastPage });
   },
 };
-
-function PageSelector(
-  props: { currentPage: number; lastPage: number; timeSelector: string },
-) {
-  return (
-    <div class="flex justify-center py-4 mx-auto">
-      <form class="inline-flex items-center gap-x-2">
-        <input type="hidden" name="time-ago" value={props.timeSelector}></input>
-        <input
-          id="current_page"
-          class={`bg-transparent rounded rounded-lg outline-none w-full border-1 border-gray-500 hover:border-black transition duration-300 disabled:(opacity-50 cursor-not-allowed) rounded-md px-2 py-1 dark:(hover:border-white)`}
-          type="number"
-          name="page"
-          min="1"
-          max={props.lastPage}
-          value={props.currentPage}
-          // @ts-ignore: this is valid HTML
-          onchange="this.form.submit()"
-        />
-        <label for="current_page" class="whitespace-nowrap align-middle">
-          of {props.lastPage}
-        </label>
-      </form>
-    </div>
-  );
-}
 
 function TimeSelector() {
   return (
--- routes/item/[id].tsx
+++ routes/item/[id].tsx
@@ -4,6 +4,7 @@ import type { State } from "@/routes/_middleware.ts";
 import Layout from "@/components/Layout.tsx";
 import Head from "@/components/Head.tsx";
 import ItemSummary from "@/components/ItemSummary.tsx";
+import PageSelector from "@/components/PageSelector.tsx";
 import {
   BUTTON_STYLES,
   INPUT_STYLES,
@@ -131,31 +132,6 @@ function CommentSummary(
         createdAt={props.comment.createdAt}
       />
       <p>{props.comment.text}</p>
-    </div>
-  );
-}
-
-function PageSelector(
-  props: { currentPage: number; lastPage: number },
-) {
-  return (
-    <div class="flex justify-center py-4 mx-auto">
-      <form class="inline-flex items-center gap-x-2">
-        <input
-          id="current_page"
-          class={`bg-transparent rounded rounded-lg outline-none w-full border-1 border-gray-500 hover:border-black transition duration-300 disabled:(opacity-50 cursor-not-allowed) rounded-md px-2 py-1 dark:(hover:border-white)`}
-          type="number"
-          name="page"
-          min="1"
-          max={props.lastPage}
-          value={props.currentPage}
-          // @ts-ignore: this is valid HTML
-          onchange="this.form.submit()"
-        />
-        <label for="current_page" class="whitespace-nowrap align-middle">
-          of {props.lastPage}
-        </label>
-      </form>
     </div>
   );
 }
