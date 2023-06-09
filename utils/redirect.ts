7bb372e10bbeef10d0009603ad91cf45cf741e21
sorakor0830 <khawsora@gmail.com>
2023-06-09 19:17:10 +0800

    add license

--- utils/redirect.ts
+++ utils/redirect.ts
@@ -1,3 +1,4 @@
+// Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { redirect } from "@/utils/http.ts";
 import { deleteCookie, getCookies, setCookie } from "std/http/cookie.ts";
 
