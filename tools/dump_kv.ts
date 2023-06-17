7db76f12dbff32f058fb6a2cc1916f2c0eb82bf1
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-06-17 16:07:42 +0200

    fix: bigint serialization error

--- tools/dump_kv.ts
+++ tools/dump_kv.ts
@@ -3,13 +3,27 @@
 // Usage: deno run -A --unstable tools/dump_kv.ts
 import { kv } from "@/utils/db.ts";
 
+interface ToReplace {
+  bigint: bigint;
+}
+
+function applyReplacer<T extends keyof ToReplace>(value: ToReplace[T]) {
+  const revivers: { [K in keyof ToReplace]?: (value: ToReplace[K]) => string } =
+    {
+      bigint: (value) => value.toString(),
+    };
+  return revivers[typeof value as T]?.(value) ?? value;
+}
+
 export async function dumpKv() {
   const iter = kv.list({ prefix: [] });
   const items = [];
   for await (const res of iter) {
     items.push({ [res.key.toString()]: res.value });
   }
-  console.log(`${JSON.stringify(items, null, 2)}`);
+  console.log(
+    `${JSON.stringify(items, (_key, value) => applyReplacer(value), 2)}`,
+  );
 }
 
 if (import.meta.main) {
