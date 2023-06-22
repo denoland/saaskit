5ee58598855b4672d513c455d4196f1dcfe62d95
Asher Gomez <ashersaupingomez@gmail.com>
2023-06-22 13:11:38 +1000

    tweak

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,6 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { Options } from "$fresh/plugins/twindv1.ts";
-import { defineConfig, Preset } from "twind/core";
+import { defineConfig, Preset } from "@twind/core";
 // twind preset
 import presetAutoPrefix from "twind-preset-autoprefix";
 import presetTailWind from "twind-preset-tailwind";
