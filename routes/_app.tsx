4171a624b1f389ccd5c8b66003a4d1542e0e1ddc
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-03 11:41:22 +1000

    partly fix conflicts

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,27 +1,16 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { AppProps } from "$fresh/server.ts";
-import Header from "../components/Header.tsx";
-import Footer from "../components/Footer.tsx";
-import { Head } from "$fresh/runtime.ts";
-import Meta from "@/components/Meta.tsx";
+import Layout from "@/components/Layout.tsx";
 
-export default function App(props: AppProps) {
+export default function App({ Component, data }: AppProps) {
   return (
-    <>
-      <Head>
-        <Meta
-          title={props.data?.title}
-          description={props.data?.description}
-          href={props.url.href}
-        />
-      </Head>
-      <div class="dark:bg-gray-900">
-        <div class="flex flex-col min-h-screen mx-auto max-w-7xl w-full dark:text-white">
-          <Header sessionId={props.data?.sessionId} />
-          <props.Component />
-          <Footer />
-        </div>
-      </div>
-    </>
+    <div>
+      <Layout
+        session={data?.sessionId}
+        hasNotifications={data?.hasNotifications}
+      >
+        <Component />
+      </Layout>
+    </div>
   );
 }
