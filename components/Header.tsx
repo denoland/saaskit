1025b48d234acecd6d15a97066c1e2dffe279342
hashrock <hashrock@users.noreply.github.com>
2023-07-14 17:01:22 +0900

    wip

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,15 +13,26 @@ export default function Header(
   props: { sessionId?: string; hasNotifications: boolean },
 ) {
   return (
-    <header class={SITE_BAR_STYLES}>
-      <a href="/">
-        <Logo height="48" />
+    <header class={SITE_BAR_STYLES + " items-start"}>
+      <input
+        type="checkbox"
+        id="menuToggle"
+        class="hidden checked:siblings:flex"
+        autoComplete="off"
+      />
+
+      <a href="/" class="block shrink-0">
+        <Logo height="48" class="shrink-0" />
       </a>
-      <nav class={NAV_STYLES}>
-        {stripe ? <a href="/pricing" class={LINK_STYLES}>Pricing</a> : null}
+      <nav
+        class={"flex flex-wrap gap-x-8 gap-y-4 items-center justify-between h-full sm:flex-row flex-col justify-normal w-full"}
+      >
+        {stripe
+          ? <a href="/pricing" class={LINK_STYLES + " block"}>Pricing</a>
+          : null}
         {props.sessionId
-          ? <a href="/account" class={LINK_STYLES}>Account</a>
-          : <a href="/signin" class={LINK_STYLES}>Sign in</a>}
+          ? <a href="/account" class={LINK_STYLES + " block"}>Account</a>
+          : <a href="/signin" class={LINK_STYLES + " block"}>Sign in</a>}
         <a
           href="/account/notifications"
           class={LINK_STYLES + " relative"}
@@ -33,6 +44,14 @@ export default function Header(
           )}
         </a>
         <a href="/submit" class={BUTTON_STYLES}>Submit</a>
+
+        <label
+          tabIndex={0}
+          class={`cursor-pointer`}
+          for="menuToggle"
+        >
+          三
+        </label>
       </nav>
     </header>
   );
