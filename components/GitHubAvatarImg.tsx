9759b06e3944c713eea81190555576233a9088d2
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-20 13:19:51 +1000

    docs and migration script

--- components/GitHubAvatarImg.tsx
+++ components/GitHubAvatarImg.tsx
@@ -1,11 +1,18 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 export default function GitHubAvatarImg(
-  props: { login: string; size: number; class?: string },
+  props: {
+    // AKA GitHub user's username.
+    login: string;
+    // Defines the rended height and width of the image, in pixels.
+    size: number;
+    class?: string;
+  },
 ) {
   return (
     <img
       height={props.size}
       width={props.size}
+      // Intrinsic size is 2x rendered size for Retina displays
       src={`https://avatars.githubusercontent.com/${props.login}?s=${
         props.size * 2
       }`}
