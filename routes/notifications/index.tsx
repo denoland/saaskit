3db5d7f53489c3819b195e596637a1ea3319fd2e
Asher Gomez <ashersaupingomez@gmail.com>
2023-07-25 08:28:40 +1000

    revert to handler method

--- routes/notifications/index.tsx
+++ routes/notifications/index.tsx
@@ -1,9 +1,22 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import type { RouteContext } from "$fresh/server.ts";
+import type { Handlers, PageProps } from "$fresh/server.ts";
 import { getNotificationsByUser, Notification } from "@/utils/db.ts";
 import { timeAgo } from "@/utils/display.ts";
 import Head from "@/components/Head.tsx";
-import { SignedInState } from "@/utils/middleware.ts";
+import type { SignedInState } from "@/utils/middleware.ts";
+
+interface NotificationsState extends SignedInState {
+  notifications: Notification[];
+}
+
+// deno-lint-ignore no-explicit-any
+export const handler: Handlers<any, NotificationsState> = {
+  async GET(_req, ctx) {
+    const notifications = await getNotificationsByUser(ctx.state.user.id);
+
+    return await ctx.render({ ...ctx.state, notifications });
+  },
+};
 
 function compareCreatedAt(a: Notification, b: Notification) {
   return Number(b.createdAt) - Number(a.createdAt);
@@ -26,23 +39,17 @@ function Row(props: Notification) {
   );
 }
 
-export default async function NotificationsPage(
-  _req: Request,
-  /** @todo Add state type variable once supported */
-  ctx: RouteContext,
+export default function NotificationsPage(
+  props: PageProps<NotificationsState>,
 ) {
-  const notifications = await getNotificationsByUser(
-    (ctx.state as unknown as SignedInState).user.id,
-  );
-
   return (
     <>
-      <Head title="Notifications" href={ctx.url.href} />
+      <Head title="Notifications" href={props.url.href} />
       <main class="flex-1 p-4">
         <h1 class="text-3xl font-bold py-4">Notifications</h1>
         <ul class="divide-y">
-          {notifications.length > 0
-            ? notifications
+          {props.data.notifications.length > 0
+            ? props.data.notifications
               .toSorted(compareCreatedAt)
               .map((notification) => <Row {...notification} />)
             : "No notifications yet"}
