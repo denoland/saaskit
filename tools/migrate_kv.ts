eca9c0f6113d583fb9da9bad9e99a5211c0c4b5b
Asher Gomez <ashersaupingomez@gmail.com>
2023-08-11 16:58:12 +1000

    tweak

--- tools/migrate_kv.ts
+++ tools/migrate_kv.ts
@@ -9,17 +9,15 @@ const targetList = [
 ];
 
 export async function migrateKv() {
-  const set = new Set();
   const iter = kv.list({ prefix: [] });
   const promises = [];
   for await (const entry of iter) {
     if (targetList.includes(entry.key[0] as string)) {
-      set.add(entry.key[0]);
       promises.push(kv.delete(entry.key));
+      console.log(entry.key);
     }
   }
   await Promise.all(promises);
-  console.log(set);
 }
 
 if (import.meta.main) {
