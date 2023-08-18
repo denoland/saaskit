982799b2199ac93bbea70bc13a6667585f4cc1c6
Asher Gomez <ashersaupingomez@gmail.com>
2023-08-18 16:23:22 +1000

    better

--- tools/migrate_kv.ts
+++ tools/migrate_kv.ts
@@ -2,9 +2,20 @@
 import { kv } from "@/utils/db.ts";
 
 export async function migrateKv() {
-  const iter = kv.list({ prefix: ["votes_count"] });
   const promises = [];
-  for await (const { key } of iter) promises.push(kv.delete(key));
+
+  const iter1 = kv.list({ prefix: ["votes"] });
+  for await (const { key } of iter1) promises.push(kv.delete(key));
+
+  const iter2 = kv.list({ prefix: ["votes_by_item"] });
+  for await (const { key } of iter2) promises.push(kv.delete(key));
+
+  const iter3 = kv.list({ prefix: ["votes_by_user"] });
+  for await (const { key } of iter3) promises.push(kv.delete(key));
+
+  const iter4 = kv.list({ prefix: ["votes_count"] });
+  for await (const { key } of iter4) promises.push(kv.delete(key));
+
   await Promise.all(promises);
   console.log("KV migration complete");
 }
