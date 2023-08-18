58ad4b98ece0d32bdd4b7bcad6b513178b6ceee0
J <2742259+javdl@users.noreply.github.com>
2023-08-18 10:05:01 +0200

    db migrations (#15)
    * Add DB migrations
    
    * fix lint errors

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,5 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import type { Handlers, PageProps } from "$fresh/server.ts";
+import type { RouteContext } from "$fresh/server.ts";
 import { calcLastPage, calcPageNum, PAGE_LENGTH } from "@/utils/pagination.ts";
 import type { State } from "./_middleware.ts";
 import ItemSummary from "@/components/ItemSummary.tsx";
@@ -13,56 +13,21 @@ import {
 } from "@/utils/db.ts";
 import { DAY, WEEK } from "std/datetime/constants.ts";
 import Head from "@/components/Head.tsx";
-import { ArrowRight, Info } from "@/components/Icons.tsx";
+// import IconInfo from "tabler_icons_tsx/info-circle.tsx";
 import { TabItem } from "@/components/TabsBar.tsx";
-
-interface HomePageData extends State {
-  items: Item[];
-  lastPage: number;
-  areVoted: boolean[];
-}
 
 const NEEDS_SETUP = Deno.env.get("GITHUB_CLIENT_ID") === undefined ||
   Deno.env.get("GITHUB_CLIENT_SECRET") === undefined;
 
+// // temporarily redirect from / to /docs
+// return new Response("", {
+//   status: 307,
+//   headers: { location: "/docs" },
+// });
+
 function calcTimeAgoFilter(url: URL) {
   return url.searchParams.get("time-ago");
 }
-
-export const handler: Handlers<HomePageData, State> = {
-  async GET(req, ctx) {
-    const slug = ctx.params.slug;
-    // temporarily redirect from / to /docs
-    return new Response("", {
-      status: 307,
-      headers: { location: "/docs" },
-    });
-
-    const url = new URL(req.url);
-    const pageNum = calcPageNum(url);
-    const timeAgo = calcTimeAgoFilter(url);
-    let allItems: Item[];
-    if (timeAgo === "week" || timeAgo === null) {
-      allItems = await getItemsSince(WEEK);
-    } else if (timeAgo === "month") {
-      allItems = await getItemsSince(30 * DAY);
-    } else {
-      allItems = await getAllItems();
-    }
-
-    const items = allItems
-      .toSorted(compareScore)
-      .slice((pageNum - 1) * PAGE_LENGTH, pageNum * PAGE_LENGTH);
-
-    const areVoted = await getAreVotedBySessionId(
-      items,
-      ctx.state.sessionId,
-    );
-    const lastPage = calcLastPage(allItems.length, PAGE_LENGTH);
-
-    return ctx.render({ ...ctx.state, items, areVoted, lastPage });
-  },
-};
 
 function TimeSelector(props: { url: URL }) {
   const timeAgo = props.url.searchParams.get("time-ago");
@@ -103,8 +68,7 @@ function SetupInstruction() {
           href="https://github.com/denoland/saaskit#auth-oauth"
           class="inline-flex gap-2 text-green-600 dark:text-green-400 hover:underline cursor-pointer"
         >
-          See the guide
-          <ArrowRight class="w-4 h-4 inline-block" />
+          See the guide ›
         </a>
       </p>
 
@@ -122,45 +86,76 @@ function SetupInstruction() {
   );
 }
 
-export default function HomePage(props: PageProps<HomePageData>) {
-  return (
-    <>
-      <Head href={props.url.href} />
-      <main class="flex-1 p-4">
-        {NEEDS_SETUP && <SetupInstruction />}
-        <TimeSelector url={props.url} />
-        {props.data.items.length === 0 && (
-          <>
-            <div class="flex flex-col justify-center items-center gap-2">
-              <div class="flex flex-col items-center gap-2 pt-16">
-                <Info class="w-10 h-10 text-gray-400 dark:text-gray-600" />
-                <p class="text-center font-medium">No items found</p>
-              </div>
+export default async function HomePage(
+  _req: Request,
+  ctx: RouteContext<undefined, State>,
+) {
+  const pageNum = calcPageNum(ctx.url);
+  const timeAgo = calcTimeAgoFilter(ctx.url);
+  let allItems: Item[];
+  if (timeAgo === "week" || timeAgo === null) {
+    allItems = await getItemsSince(WEEK);
+  } else if (timeAgo === "month") {
+    allItems = await getItemsSince(30 * DAY);
+  } else {
+    allItems = await getAllItems();
+  }
 
-              <a
-                href="/submit"
-                class="inline-flex items-center justify-center gap-2 rounded-md px-3 py-2 text-primary hover:underline"
-              >
-                Submit your project
-              </a>
-            </div>
-          </>
-        )}
+  const items = allItems
+    .toSorted(compareScore)
+    .slice((pageNum - 1) * PAGE_LENGTH, pageNum * PAGE_LENGTH);
 
-        {props.data.items.map((item, index) => (
-          <ItemSummary
-            item={item}
-            isVoted={props.data.areVoted[index]}
-          />
-        ))}
-        {props.data.lastPage > 1 && (
-          <PageSelector
-            currentPage={calcPageNum(props.url)}
-            lastPage={props.data.lastPage}
-            timeSelector={calcTimeAgoFilter(props.url) ?? undefined}
-          />
-        )}
-      </main>
-    </>
+  const areVoted = await getAreVotedBySessionId(
+    items,
+    ctx.state.sessionId,
   );
+  const lastPage = calcLastPage(allItems.length, PAGE_LENGTH);
+
+  // temporarily redirect from / to /docs
+  return new Response("", {
+    status: 307,
+    headers: { location: "/docs" },
+  });
+
+  // return (
+
+  //   <>
+  //     <Head href={ctx.url.href} />
+  //     <main class="flex-1 p-4">
+  //       {NEEDS_SETUP && <SetupInstruction />}
+  //       <TimeSelector url={ctx.url} />
+  //       {items.length === 0 && (
+  //         <>
+  //           <div class="flex flex-col justify-center items-center gap-2">
+  //             <div class="flex flex-col items-center gap-2 pt-16">
+  //               {/* <IconInfo class="w-10 h-10 text-gray-400 dark:text-gray-600" /> */}
+  //               <p class="text-center font-medium">No items found</p>
+  //             </div>
+
+  //             <a
+  //               href="/submit"
+  //               class="inline-flex items-center justify-center gap-2 rounded-md px-3 py-2 text-primary hover:underline"
+  //             >
+  //               Submit your project
+  //             </a>
+  //           </div>
+  //         </>
+  //       )}
+
+  //       {items.map((item, index) => (
+  //         <ItemSummary
+  //           item={item}
+  //           isVoted={areVoted[index]}
+  //         />
+  //       ))}
+  //       {lastPage > 1 && (
+  //         <PageSelector
+  //           currentPage={calcPageNum(ctx.url)}
+  //           lastPage={lastPage}
+  //           timeSelector={calcTimeAgoFilter(ctx.url) ?? undefined}
+  //         />
+  //       )}
+  //     </main>
+  //   </>
+  // );
 }
