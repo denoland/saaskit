17a175e7efcb11e3fc811d553c536f94cf51f3da
Asher Gomez <ashersaupingomez@gmail.com>
2023-08-20 21:40:57 +1000

    feat: load more comments

--- islands/CommentsList.tsx
+++ islands/CommentsList.tsx
@@ -1,45 +1,55 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { useSignal } from "@preact/signals";
-import { useEffect, useRef } from "preact/hooks";
+import { useEffect } from "preact/hooks";
 import { Comment } from "@/utils/db.ts";
-import CommentSummary from "@/components/CommentSummary.tsx";
+import UserPostedAt from "@/components/UserPostedAt.tsx";
+import { LINK_STYLES } from "@/utils/constants.ts";
 
 async function fetchComments(itemId: string, cursor: string) {
   let url = `/api/items/${itemId}/comments`;
-  if (cursor !== "") url += "?cursor=" + cursor;
+  if (cursor !== "" && cursor !== undefined) url += "?cursor=" + cursor;
 
   const resp = await fetch(url);
   if (!resp.ok) throw new Error(`Request failed: GET ${url}`);
-  return await resp.json() as { comments: Comment[]; cursor: string };
+  return await resp.json() as { newComments: Comment[]; newCursor: string };
+}
+
+function CommentSummary(props: Comment) {
+  return (
+    <div class="py-4">
+      <UserPostedAt {...props} />
+      <p>{props.text}</p>
+    </div>
+  );
 }
 
 export default function CommentsList(props: { itemId: string }) {
-  const commentsSig = useSignal<Comment[]>([]);
-  const cursorSig = useSignal("");
-  const bottomRef = useRef<HTMLDivElement>(null);
+  const comments = useSignal<Comment[]>([]);
+  const cursor = useSignal("");
+  const isLoading = useSignal(false);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) {
-        fetchComments(props.itemId, cursorSig.value)
-          .then(({ comments, cursor }) => {
-            if (cursor === "") observer.unobserve(entry.target);
-            commentsSig.value = [...commentsSig.value, ...comments];
-            cursorSig.value = cursor;
-          });
-      }
-    });
+  function loadMoreComments() {
+    isLoading.value = true;
+    fetchComments(props.itemId, cursor.value)
+      .then(({ newComments, newCursor }) => {
+        comments.value = [...comments.value, ...newComments];
+        cursor.value = newCursor;
+        isLoading.value = false;
+      });
+  }
 
-    observer.observe(bottomRef.current!);
-    return () => observer.disconnect();
-  }, []);
+  useEffect(loadMoreComments, []);
 
   return (
     <div>
-      {commentsSig.value.map((comment) => (
+      {comments.value.map((comment) => (
         <CommentSummary key={comment.id} {...comment} />
       ))}
-      <div ref={bottomRef}></div>
+      {cursor.value !== "" && !isLoading.value && (
+        <button onClick={loadMoreComments} class={LINK_STYLES}>
+          Load more
+        </button>
+      )}
     </div>
   );
 }
