4208b8e0b17070b86eed4287d82aeee157c76500
Asher Gomez <ashersaupingomez@gmail.com>
2023-08-21 08:45:43 +1000

    better

--- islands/CommentsList.tsx
+++ islands/CommentsList.tsx
@@ -1,71 +1,79 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { useSignal } from "@preact/signals";
-import { useCallback, useEffect, useRef } from "preact/hooks";
-import { Ref } from "preact";
+import { useEffect, useRef } from "preact/hooks";
 import { Comment } from "@/utils/db.ts";
 import UserPostedAt from "@/components/UserPostedAt.tsx";
 
 async function fetchComments(itemId: string, cursor: string) {
-  let url = `/api/items/${itemId}/comments`;
-  if (cursor !== "") url += "?cursor=" + cursor;
-
+  const url = `/api/items/${itemId}/comments${
+    cursor ? `?cursor=${cursor}` : ""
+  }`;
   const resp = await fetch(url);
   if (!resp.ok) throw new Error(`Request failed: GET ${url}`);
   return await resp.json() as { comments: Comment[]; cursor: string };
 }
 
+function CommentSummary(props: Comment) {
+  return (
+    <div class="py-4">
+      <UserPostedAt {...props} />
+      <p>{props.text}</p>
+    </div>
+  );
+}
+
 export default function CommentsList(props: { itemId: string }) {
   const commentsSig = useSignal<Comment[]>([]);
-  const cursorSig = useSignal("");
+  const cursorSig = useSignal<string>("");
   const observer = useRef<IntersectionObserver>();
-  const lastElementRef = useCallback((node: HTMLDivElement) => {
-    if (!cursorSig.value) return;
-    if (observer.current) observer.current.disconnect();
+  const lastElementRef = useRef<HTMLDivElement>(null);
 
-    observer.current = new IntersectionObserver(async ([entry]) => {
-      if (entry.isIntersecting) {
-        const { comments, cursor } = await fetchComments(
-          props.itemId,
-          cursorSig.value,
-        );
+  async function loadMoreComments() {
+    if (cursorSig.value === "") return;
 
-        if (cursor === "") observer.current?.unobserve(entry.target);
+    const { comments, cursor } = await fetchComments(
+      props.itemId,
+      cursorSig.value,
+    );
 
-        commentsSig.value = [...commentsSig.value, ...comments];
-        cursorSig.value = cursor;
-      }
-    });
+    if (cursor === "") observer.current?.disconnect();
 
-    if (node) observer.current.observe(node);
-  }, [cursorSig.value]);
+    commentsSig.value = [...commentsSig.value, ...comments];
+    cursorSig.value = cursor;
+  }
 
   useEffect(() => {
-    fetchComments(props.itemId, cursorSig.value).then(
-      ({ comments, cursor }) => {
+    fetchComments(props.itemId, cursorSig.value)
+      .then(({ comments, cursor }) => {
         commentsSig.value = comments;
         cursorSig.value = cursor;
-      },
-    );
+      });
   }, []);
+
+  useEffect(() => {
+    if (!observer.current) {
+      observer.current = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+          loadMoreComments();
+        }
+      });
+    }
+
+    if (cursorSig.value) {
+      observer.current.observe(lastElementRef.current!);
+    }
+
+    return () => {
+      observer.current?.disconnect();
+    };
+  }, [cursorSig.value]);
 
   return (
     <div>
-      {commentsSig.value.map((comment) => {
-        const isLast =
-          comment.id === commentsSig.value[commentsSig.value.length - 1].id;
-        const props = {
-          class: "py-4",
-          key: comment.id,
-          ref: isLast ? lastElementRef as Ref<HTMLDivElement> : null,
-        };
-
-        return (
-          <div {...props}>
-            <UserPostedAt {...comment} />
-            <p>{comment.text}</p>
-          </div>
-        );
-      })}
+      {commentsSig.value.map((comment) => (
+        <CommentSummary key={comment.id} {...comment} />
+      ))}
+      <div ref={lastElementRef} />
     </div>
   );
 }
