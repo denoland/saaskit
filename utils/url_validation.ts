cd4b4ed8410acefb9fe533f9215bfc285a57ca4e
Michael Herzner <michael.b.herzner@gmail.com>
2023-08-22 22:58:45 +0200

    feat: add url validation

---
+++ utils/url_validation.ts
@@ -0,0 +1,30 @@
+export function isValidUrl(string: string): boolean {
+  try {
+    const { protocol } = new URL(string);
+    const pattern = /^(https?):/
+
+    return pattern.test(protocol);
+  } catch (_) {
+    return false;
+  }
+}
+
+export function isPublicUrl(string: string): boolean {
+  try {
+    const { hostname } = new URL(string);
+    const ranges = [
+      /^localhost$/,
+      /^127\.\d{1,3}\.\d{1,3}\.\d{1,3}$/,
+      /^::1$/,
+      /^0:0:0:0:0:0:0:1$/,
+      /^10\.\d{1,3}\.\d{1,3}\.\d{1,3}$/,
+      /^172\.(1[6-9]|2\d|3[0-1])\.\d{1,3}\.\d{1,3}$/,
+      /^192\.168\.\d{1,3}\.\d{1,3}$/,
+    ];
+
+    return !ranges.some((range) => range.test(hostname));
+  } catch (_) {
+    return false;
+  }
+  
+}
