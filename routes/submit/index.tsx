24dc08c80107baaaba1e0d34152712e0fc20135e
Asher Gomez <ashersaupingomez@gmail.com>
2023-08-29 10:38:15 +1000

    Apply suggestions from code review

--- routes/submit/index.tsx
+++ routes/submit/index.tsx
@@ -22,27 +22,27 @@ export const handler: Handlers<SignedInState, SignedInState> = {
     const url = form.get("url");
 
     if (typeof title !== "string" || typeof url !== "string") {
-      return new Response("URL or Title are either invalid or missing.", {
+      return new Response("Item URL or title are either invalid or missing.", {
         status: 400,
       });
     }
 
     try {
       if (!isValidUrl(url) || !isPublicUrl(url)) {
-        return new Response("URL must be valid and publicly available.", {
+        return new Response("Item URL must be valid and publicly available.", {
           status: 400,
         });
       }
     } catch {
-      return new Response("An Error occurred while validating the URL.", {
+      return new Response("An error occurred while validating the URL.", {
         status: 500,
       });
     }
 
     const user = await getUserBySession(ctx.state.sessionId);
 
-    if (!user) {
-      return new Response("You must be logged in to submit a new Item.", {
+    if (user === null) {
+      return new Response("You must be logged in to submit a new item.", {
         status: 401,
       });
     }
@@ -51,7 +51,7 @@ export const handler: Handlers<SignedInState, SignedInState> = {
 
     if (items.some((item) => item.url === url || item.title === title)) {
       return new Response(
-        "You already submitted an Item which contains the same Title or URL.",
+        "You already submitted an item which contains the same title or URL.",
         { status: 409 },
       );
     }
