546b90a4e0062797e4aa7da95eff5e4001dcbbeb
Michael Herzner <michael.b.herzner@gmail.com>
2023-09-05 22:55:22 +0200

    chore: implement review remarks

--- routes/items/[id].tsx
+++ routes/items/[id].tsx
@@ -7,19 +7,19 @@ import Head from "@/components/Head.tsx";
 import type { State } from "@/middleware/session.ts";
 import CommentsList from "@/islands/CommentsList.tsx";
 
-function CommentInput(props: { isLoggedIn: boolean; itemId: string }) {
+function CommentInput(props: { isSignedIn: boolean; itemId: string }) {
   return (
     <form method="post" action="/api/comments">
       <input type="hidden" name="item_id" value={props.itemId} />
       <textarea
         class={`${INPUT_STYLES} w-full`}
-        disabled={!props.isLoggedIn}
+        disabled={!props.isSignedIn}
         type="text"
         name="text"
         required
       />
-      <button class={BUTTON_STYLES} disabled={!props.isLoggedIn} type="submit">
-        {props.isLoggedIn ? "Comment" : "Sign in to comment"}
+      <button class={BUTTON_STYLES} disabled={!props.isSignedIn} type="submit">
+        {props.isSignedIn ? "Comment" : "Sign in to comment"}
       </button>
     </form>
   );
@@ -33,10 +33,10 @@ export default async function ItemsItemPage(
   const item = await getItem(itemId);
   if (item === null) return await ctx.renderNotFound();
 
-  const isLoggedIn = ctx.state.sessionUser !== undefined;
+  const isSignedIn = ctx.state.sessionUser !== undefined;
   let isVoted = false;
 
-  if (isLoggedIn) {
+  if (isSignedIn) {
     const areVoted = await getAreVotedByUser(
       [item],
       ctx.state.sessionUser!.login,
@@ -52,7 +52,7 @@ export default async function ItemsItemPage(
           item={item}
           isVoted={isVoted}
         />
-        <CommentInput isLoggedIn={isLoggedIn} itemId={ctx.params.id} />
+        <CommentInput isSignedIn={isSignedIn} itemId={ctx.params.id} />
         <CommentsList itemId={ctx.params.id} />
       </main>
     </>
