60338dfee64832e63616fb2fd935da8824cbe817
Asher Gomez <ashersaupingomez@gmail.com>
2023-09-07 18:58:49 +1000

    refactor: polish posts logic

--- utils/posts_test.ts
+++ utils/posts_test.ts
@@ -4,30 +4,15 @@ import { getPost, getPosts } from "./posts.ts";
 import { assert, assertEquals } from "std/assert/mod.ts";
 
 Deno.test("[blog] getPost()", async () => {
-  const post = await getPost("first-post");
+  const post = await getPost("first-post.md");
   assert(post);
   assertEquals(post.publishedAt, new Date("2022-11-04T15:00:00.000Z"));
   assertEquals(post.summary, "This is an excerpt of my first blog post.");
   assertEquals(post.title, "This is my first blog post!");
+  assertEquals(await getPost("third-post.md"), null);
 });
 
-Deno.test("[blog] getPost() with missing frontmatter attributes", async () => {
-  const post = await getPost("second-post");
-  assert(post);
-  assertEquals(post.publishedAt, new Date("2022-11-04T15:00:00.000Z"));
-  assertEquals(
-    post.summary,
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  );
-  assertEquals(post.title, "Second post");
-});
-
-Deno.test("[blog] getPost() for non-existent post", async () => {
-  const post = await getPost("third-post");
-  assertEquals(post, null);
-});
-
-Deno.test("[blog] getPosts() from data directory", async () => {
+Deno.test("[blog] getPosts()", async () => {
   const posts = await getPosts();
   assert(posts);
   assertEquals(posts.length, 2);
