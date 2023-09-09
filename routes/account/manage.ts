c0a81d60da42580741e6daf66b7a0d85a476c65d
Josh Gum <revgum@gmail.com>
2023-09-09 05:46:27 -0700

    refactor: account page config and error handling

--- routes/account/manage.ts
+++ routes/account/manage.ts
@@ -3,21 +3,14 @@ import type { RouteContext } from "$fresh/server.ts";
 import { stripe } from "@/utils/stripe.ts";
 import type { SignedInState } from "@/middleware/session.ts";
 import { redirect } from "@/utils/http.ts";
-import { createHttpError } from "std/http/http_errors.ts";
-import { Status } from "std/http/http_status.ts";
 
 export default async function AccountManagePage(
   _req: Request,
   ctx: RouteContext<undefined, SignedInState>,
 ) {
-  if (stripe === undefined) throw createHttpError(Status.NotFound);
-
   const { sessionUser } = ctx.state;
-  if (sessionUser.stripeCustomerId === undefined) {
-    throw createHttpError(
-      Status.NotFound,
-      "User does not have a Stripe customer ID",
-    );
+  if (stripe === undefined || sessionUser.stripeCustomerId === undefined) {
+    return ctx.renderNotFound();
   }
 
   const { url } = await stripe.billingPortal.sessions.create({
