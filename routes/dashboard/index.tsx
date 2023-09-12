93cb969541d3a6d1effe8757e37a61ea70c0bea6
harshmangalam <harshdev8218@gmail.com>
2023-09-12 12:01:40 +0530

    added route helper fresh-1.4

--- routes/dashboard/index.tsx
+++ routes/dashboard/index.tsx
@@ -1,7 +1,7 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { redirect } from "@/utils/http.ts";
+import { defineRoute } from "$fresh/src/server/defines.ts";
 
-// deno-lint-ignore require-await
-export default async function DashboardPage() {
+export default defineRoute(() => {
   return redirect("/dashboard/stats");
-}
+});
