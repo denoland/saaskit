56ecab78c845e3328e7f021217d6b9158051bb22
harshmangalam <harshdev8218@gmail.com>
2023-09-12 11:50:26 +0530

    added app helper fresh-1.4

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,18 +3,16 @@ import { AppProps } from "$fresh/server.ts";
 import Header from "@/components/Header.tsx";
 import Footer from "@/components/Footer.tsx";
 import type { State } from "@/middleware/session.ts";
+import { defineApp } from "$fresh/src/server/defines.ts";
 
-export default function App(props: AppProps<undefined, State>) {
+export default defineApp<State>((_, ctx) => {
   return (
     <div class="dark:bg-gray-900">
       <div class="flex flex-col min-h-screen mx-auto max-w-7xl w-full dark:text-white">
-        <Header
-          url={props.url}
-          sessionUser={props.state?.sessionUser}
-        />
-        <props.Component />
-        <Footer url={props.url} />
+        <Header url={ctx.url} sessionUser={ctx.state?.sessionUser} />
+        <ctx.Component />
+        <Footer url={ctx.url} />
       </div>
     </div>
   );
-}
+});
