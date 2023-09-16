7cd313a5d6bffb08229b11b0206745294fba5e0f
Javier R <javier.rios-urbano@epitech.eu>
2023-09-16 18:46:15 +0200

    add: `hardenHeaders` middleware

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,6 +1,8 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { setSessionState } from "@/middleware/session.ts";
+import { hardenHeaders } from "@/middleware/headers.ts";
 
 export const handler = [
   setSessionState,
+  hardenHeaders,
 ];
