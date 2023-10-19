f356535a1cb32648825d1362364f9327988e0950
Asher Gomez <ashersaupingomez@gmail.com>
2023-10-19 13:45:40 +0900

    refactor: use Tailwind plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,19 +1,28 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
 import kvOAuthPlugin from "./plugins/kv_oauth.ts";
 import sessionPlugin from "./plugins/session.ts";
 import errorHandling from "./plugins/error_handling.ts";
 import securityHeaders from "./plugins/security_headers.ts";
 import welcomePlugin from "./plugins/welcome.ts";
 import { FreshOptions } from "$fresh/server.ts";
+import tailwindPlugin from "$fresh/plugins/tailwind.ts";
 
 export default {
   plugins: [
     welcomePlugin,
     kvOAuthPlugin,
     sessionPlugin,
-    twindPlugin(twindConfig),
+    tailwindPlugin({
+      css: "./styles.css",
+      mode: "build",
+      dest: "./static/styles",
+    }, {
+      content: [
+        "./routes/**/*.{ts,tsx}",
+        "./islands/**/*.tsx",
+        "./components/**/*.tsx",
+      ],
+    }),
     errorHandling,
     securityHeaders,
   ],
