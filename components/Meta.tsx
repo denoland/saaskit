05ae71de3137f52cac75a5d7e9c8f3ecdb7e772a
Asher Gomez <ashersaupingomez@gmail.com>
2024-01-31 11:22:32 +1100

    chore: remove redundant meta tag

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -26,7 +26,6 @@ export default function Meta(props: MetaProps) {
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={props.title} />
       <meta property="og:locale" content="en" />
-      <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title} />
       <meta property="og:description" content={props.description} />
       <meta property="og:url" content={props.href} />
