denoland/std

commit 27320c57d8ad169de3fd9b0a4ef3c7810c63572a

chore: upgrade to Fresh 1.6.1 and std 0.208.0 (#646)
closes https://github.com/denoland/saaskit/issues/644

I see the guidelines have changed since I last contributed (thanks
github!) and that caused me to look for an issue. Fortunately there was
the above, but I see you want tailwind as well. I'll open this as a
draft for now.

---------

Co-authored-by: Asher Gomez <ashersaupingomez@gmail.com>

components/Header.tsx
@@ -1,15 +1,8 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import {
-  ACTIVE_ANCESTOR_LINK_STYLES,
-  ACTIVE_LINK_STYLES,
-  LINK_STYLES,
-  SITE_BAR_STYLES,
-  SITE_NAME,
-} from "@/utils/constants.ts";
+import { SITE_NAME } from "@/utils/constants.ts";
 import { isStripeEnabled } from "@/utils/stripe.ts";
 import IconX from "tabler_icons_tsx/x.tsx";
 import IconMenu from "tabler_icons_tsx/menu-2.tsx";
-import { cx } from "@twind/core";
 import { User } from "@/utils/db.ts";
 
 export interface HeaderProps {
@@ -23,14 +16,8 @@ export interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps) {
-  const NAV_ITEM = "text-gray-500 px-3 py-4 sm:py-2";
   return (
-    <header
-      class={cx(
-        SITE_BAR_STYLES,
-        "flex-col sm:flex-row",
-      )}
-    >
+    <header class="site-bar-styles flex-col sm:flex-row">
       <input
         type="checkbox"
         id="nav-toggle"
@@ -71,11 +58,11 @@ export default function Header(props: HeaderProps) {
         `}
       </script>
       <nav
-        class={"hidden flex-col gap-x-4 divide-y divide-solid sm:(flex items-center flex-row divide-y-0)"}
+        class={"hidden flex-col gap-x-4 divide-y divide-solid sm:flex sm:items-center sm:flex-row sm:divide-y-0"}
       >
         <a
           href="/dashboard"
-          class={cx(LINK_STYLES, ACTIVE_ANCESTOR_LINK_STYLES, NAV_ITEM)}
+          class="link-styles data-[ancestor]:!text-black data-[ancestor]:dark:!text-white nav-item"
         >
           Dashboard
         </a>
@@ -83,7 +70,7 @@ export default function Header(props: HeaderProps) {
           (
             <a
               href="/pricing"
-              class={cx(LINK_STYLES, ACTIVE_LINK_STYLES, NAV_ITEM)}
+              class="link-styles data-[current]:!text-black data-[current]:dark:!text-white nav-item"
             >
               Pricing
             </a>
@@ -92,16 +79,20 @@ export default function Header(props: HeaderProps) {
           ? (
             <a
               href="/account"
-              class={cx(LINK_STYLES, ACTIVE_LINK_STYLES, NAV_ITEM)}
+              class="link-styles data-[current]:!text-black data-[current]:dark:!text-white nav-item"
             >
               Account
             </a>
           )
-          : <a href="/signin" class={cx(LINK_STYLES, NAV_ITEM)}>Sign in</a>}
+          : (
+            <a href="/signin" class="link-styles nav-item">
+              Sign in
+            </a>
+          )}
         <div class="rounded-lg bg-gradient-to-tr from-secondary to-primary p-px">
           <a
             href="/submit"
-            class="text-center text-white rounded-[7px] transition duration-300 px-4 py-2 block hover:(bg-white text-black dark:(bg-gray-900 !text-white))"
+            class="text-center text-white rounded-[7px] transition duration-300 px-4 py-2 block hover:bg-white hover:text-black hover:dark:bg-gray-900 hover:dark:!text-white"
           >
             Submit
           </a>

components/Footer.tsx
@@ -1,12 +1,5 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import {
-  ACTIVE_LINK_STYLES,
-  LINK_STYLES,
-  NAV_STYLES,
-  SITE_BAR_STYLES,
-  SITE_NAME,
-} from "@/utils/constants.ts";
-import { cx } from "@twind/core";
+import { SITE_NAME } from "@/utils/constants.ts";
 import IconBrandDiscord from "tabler_icons_tsx/brand-discord.tsx";
 import IconBrandGithub from "tabler_icons_tsx/brand-github.tsx";
 import IconRss from "tabler_icons_tsx/rss.tsx";
@@ -66,25 +59,23 @@ export interface FooterProps {
 
 export default function Footer(props: FooterProps) {
   return (
-    <footer
-      class={`${SITE_BAR_STYLES} flex-col md:flex-row mt-8`}
-    >
+    <footer class="site-bar-styles flex-col md:flex-row mt-8">
       <p>© {SITE_NAME}</p>
-      <nav class={NAV_STYLES}>
+      <nav class="nav-styles">
         <a
           href="/blog"
-          class={cx(LINK_STYLES, ACTIVE_LINK_STYLES)}
+          class="link-styles data-[current]:!text-black data-[current]:dark:!text-white"
         >
           Blog
         </a>
-        <a href="/feed" aria-label="Deno Hunt RSS Feed" class={LINK_STYLES}>
+        <a href="/feed" aria-label="Deno Hunt RSS Feed" class="link-styles">
           <IconRss class="h-6 w-6" />
         </a>
         <a
           href="https://discord.gg/deno"
           target="_blank"
           aria-label="Deno SaaSKit on Discord"
-          class={LINK_STYLES}
+          class="link-styles"
         >
           <IconBrandDiscord class="h-6 w-6" />
         </a>
@@ -92,7 +83,7 @@ export default function Footer(props: FooterProps) {
           href="https://github.com/denoland/saaskit"
           target="_blank"
           aria-label="Deno SaaSKit repo on GitHub"
-          class={LINK_STYLES}
+          class="link-styles"
         >
           <IconBrandGithub class="h-6 w-6" />
         </a>

components/TabsBar.tsx
@@ -1,6 +1,4 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import { LINK_STYLES } from "@/utils/constants.ts";
-import { cx } from "@twind/core";
 import { ComponentChildren } from "preact";
 
 export interface TabItemProps {
@@ -15,13 +13,11 @@ export function TabItem(props: TabItemProps) {
   return (
     <a
       href={props.path}
-      class={cx(
-        "px-4 py-2 rounded-lg",
+      class={`px-4 py-2 rounded-lg ${
         props.active
-          ? "bg-gray-100 text-black dark:(bg-gray-800 text-white)"
-          : "",
-        LINK_STYLES,
-      )}
+          ? "bg-gray-100 text-black dark:bg-gray-800 dark:text-white"
+          : ""
+      } link-styles`}
     >
       {props.children}
     </a>

e2e_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 
-import { createHandler, Status } from "$fresh/server.ts";
+import { createHandler } from "$fresh/server.ts";
 import manifest from "@/fresh.gen.ts";
 import {
   collectValues,
@@ -24,7 +24,7 @@ import {
   assertObjectMatch,
   assertStringIncludes,
 } from "std/assert/mod.ts";
-import { isRedirectStatus } from "std/http/status.ts";
+import { isRedirectStatus, STATUS_CODE } from "std/http/status.ts";
 import { resolvesNext, returnsNext, stub } from "std/testing/mock.ts";
 import Stripe from "stripe";
 import options from "./fresh.config.ts";
@@ -94,7 +94,7 @@ Deno.test("[e2e] security headers", async () => {
 Deno.test("[e2e] GET /", async () => {
   const resp = await handler(new Request("http://localhost"));
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertHtml(resp);
 });
 
@@ -184,7 +184,7 @@ Deno.test("[e2e] GET /blog", async () => {
     new Request("http://localhost/blog"),
   );
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertHtml(resp);
 });
 
@@ -192,7 +192,7 @@ Deno.test("[e2e] GET /pricing", async () => {
   const req = new Request("http://localhost/pricing");
   const resp = await handler(req);
 
-  assertEquals(resp.status, Status.NotFound);
+  assertEquals(resp.status, STATUS_CODE.NotFound);
   assertHtml(resp);
 });
 
@@ -255,7 +255,7 @@ Deno.test("[e2e] GET /dashboard/stats", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertHtml(resp);
     assertStringIncludes(await resp.text(), "<!--frsh-chart_default");
   });
@@ -279,7 +279,7 @@ Deno.test("[e2e] GET /dashboard/users", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertHtml(resp);
     assertStringIncludes(await resp.text(), "<!--frsh-userstable_default");
   });
@@ -290,7 +290,7 @@ Deno.test("[e2e] GET /submit", async () => {
     new Request("http://localhost/submit"),
   );
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertHtml(resp);
 });
 
@@ -299,7 +299,7 @@ Deno.test("[e2e] GET /feed", async () => {
     new Request("http://localhost/feed"),
   );
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertXml(resp);
 });
 
@@ -312,7 +312,7 @@ Deno.test("[e2e] GET /api/items", async () => {
   const resp = await handler(req);
   const { values } = await resp.json();
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertJson(resp);
   assertArrayIncludes(values, [item1, item2]);
 });
@@ -391,7 +391,7 @@ Deno.test("[e2e] GET /api/items/[id]", async (test) => {
   await test.step("serves not found response if item not found", async () => {
     const resp = await handler(req);
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertEquals(await resp.text(), "Item not found");
   });
 
@@ -399,7 +399,7 @@ Deno.test("[e2e] GET /api/items/[id]", async (test) => {
     await createItem(item);
     const resp = await handler(req);
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertJson(resp);
     assertEquals(await resp.json(), item);
   });
@@ -416,7 +416,7 @@ Deno.test("[e2e] GET /api/users", async () => {
 
   const { values } = await resp.json();
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertJson(resp);
   assertArrayIncludes(values, [user1, user2]);
 });
@@ -428,7 +428,7 @@ Deno.test("[e2e] GET /api/users/[login]", async (test) => {
   await test.step("serves not found response if user not found", async () => {
     const resp = await handler(req);
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertText(resp);
     assertEquals(await resp.text(), "User not found");
   });
@@ -437,7 +437,7 @@ Deno.test("[e2e] GET /api/users/[login]", async (test) => {
     await createUser(user);
     const resp = await handler(req);
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertJson(resp);
     assertEquals(await resp.json(), user);
   });
@@ -454,7 +454,7 @@ Deno.test("[e2e] GET /api/users/[login]/items", async (test) => {
   await test.step("serves not found response if user not found", async () => {
     const resp = await handler(req);
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertText(resp);
     assertEquals(await resp.text(), "User not found");
   });
@@ -465,7 +465,7 @@ Deno.test("[e2e] GET /api/users/[login]/items", async (test) => {
     const resp = await handler(req);
     const { values } = await resp.json();
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertJson(resp);
     assertArrayIncludes(values, [item]);
   });
@@ -481,7 +481,7 @@ Deno.test("[e2e] POST /api/vote", async (test) => {
   await test.step("serves unauthorized response if the session user is not signed in", async () => {
     const resp = await handler(new Request(url, { method: "POST" }));
 
-    assertEquals(resp.status, Status.Unauthorized);
+    assertEquals(resp.status, STATUS_CODE.Unauthorized);
     assertText(resp);
     assertEquals(await resp.text(), "User must be signed in");
   });
@@ -494,7 +494,7 @@ Deno.test("[e2e] POST /api/vote", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertText(resp);
     assertEquals(await resp.text(), "Item not found");
   });
@@ -509,7 +509,7 @@ Deno.test("[e2e] POST /api/vote", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.Created);
+    assertEquals(resp.status, STATUS_CODE.Created);
   });
 
   await test.step("serves an error response if the `item_id` URL parameter is missing", async () => {
@@ -520,7 +520,7 @@ Deno.test("[e2e] POST /api/vote", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.BadRequest);
+    assertEquals(resp.status, STATUS_CODE.BadRequest);
     assertEquals(await resp.text(), "`item_id` URL parameter missing");
   });
 });
@@ -553,7 +553,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
     Deno.env.delete("STRIPE_SECRET_KEY");
     const resp = await handler(new Request(url, { method: "POST" }));
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertText(resp);
     assertEquals(await resp.text(), "Not Found");
   });
@@ -562,7 +562,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
     Deno.env.set("STRIPE_SECRET_KEY", crypto.randomUUID());
     const resp = await handler(new Request(url, { method: "POST" }));
 
-    assertEquals(resp.status, Status.BadRequest);
+    assertEquals(resp.status, STATUS_CODE.BadRequest);
     assertText(resp);
     assertEquals(await resp.text(), "`Stripe-Signature` header is missing");
   });
@@ -576,7 +576,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.InternalServerError);
+    assertEquals(resp.status, STATUS_CODE.InternalServerError);
     assertText(resp);
     assertEquals(
       await resp.text(),
@@ -593,7 +593,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.BadRequest);
+    assertEquals(resp.status, STATUS_CODE.BadRequest);
     assertText(resp);
     assertEquals(
       await resp.text(),
@@ -619,7 +619,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
 
     constructEventAsyncStub.restore();
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertText(resp);
     assertEquals(await resp.text(), "User not found");
   });
@@ -648,7 +648,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
 
     constructEventAsyncStub.restore();
 
-    assertEquals(resp.status, Status.Created);
+    assertEquals(resp.status, STATUS_CODE.Created);
     assertEquals(await getUser(user.login), { ...user, isSubscribed: true });
   });
 
@@ -670,7 +670,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
 
     constructEventAsyncStub.restore();
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertText(resp);
     assertEquals(await resp.text(), "User not found");
   });
@@ -700,7 +700,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
     constructEventAsyncStub.restore();
 
     assertEquals(await getUser(user.login), { ...user, isSubscribed: false });
-    assertEquals(resp.status, Status.Accepted);
+    assertEquals(resp.status, STATUS_CODE.Accepted);
   });
 
   await test.step("serves bad request response if the event type is not supported", async () => {
@@ -721,7 +721,7 @@ Deno.test("[e2e] POST /api/stripe-webhooks", async (test) => {
 
     constructEventAsyncStub.restore();
 
-    assertEquals(resp.status, Status.BadRequest);
+    assertEquals(resp.status, STATUS_CODE.BadRequest);
     assertText(resp);
     assertEquals(await resp.text(), "Event type not supported");
   });
@@ -746,7 +746,7 @@ Deno.test("[e2e] GET /account", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertHtml(resp);
     assertStringIncludes(await resp.text(), 'href="/account/upgrade"');
   });
@@ -761,7 +761,7 @@ Deno.test("[e2e] GET /account", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.OK);
+    assertEquals(resp.status, STATUS_CODE.OK);
     assertHtml(resp);
     assertStringIncludes(await resp.text(), 'href="/account/manage"');
   });
@@ -786,7 +786,7 @@ Deno.test("[e2e] GET /account/manage", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertHtml(resp);
   });
 
@@ -836,7 +836,7 @@ Deno.test("[e2e] GET /account/upgrade", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.InternalServerError);
+    assertEquals(resp.status, STATUS_CODE.InternalServerError);
     assertHtml(resp);
   });
 
@@ -849,7 +849,7 @@ Deno.test("[e2e] GET /account/upgrade", async (test) => {
       }),
     );
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertHtml(resp);
   });
 
@@ -874,7 +874,7 @@ Deno.test("[e2e] GET /account/upgrade", async (test) => {
 
     sessionsCreateStub.restore();
 
-    assertEquals(resp.status, Status.NotFound);
+    assertEquals(resp.status, STATUS_CODE.NotFound);
     assertHtml(resp);
   });
 
@@ -926,7 +926,7 @@ Deno.test("[e2e] GET /api/me/votes", async () => {
   );
   const body = await resp.json();
 
-  assertEquals(resp.status, Status.OK);
+  assertEquals(resp.status, STATUS_CODE.OK);
   assertJson(resp);
   assertArrayIncludes(body, [{ ...item1, score: 1 }, { ...item2, score: 1 }]);
 });

fresh.config.ts
@@ -1,6 +1,5 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
 import kvOAuthPlugin from "./plugins/kv_oauth.ts";
 import sessionPlugin from "./plugins/session.ts";
 import errorHandling from "./plugins/error_handling.ts";
@@ -13,7 +12,7 @@ export default {
     welcomePlugin,
     kvOAuthPlugin,
     sessionPlugin,
-    twindPlugin(twindConfig),
+    tailwind(),
     errorHandling,
     securityHeaders,
   ],

fresh.gen.ts
@@ -2,69 +2,70 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_500.tsx";
-import * as $2 from "./routes/_app.tsx";
-import * as $3 from "./routes/account/index.tsx";
-import * as $4 from "./routes/account/manage.ts";
-import * as $5 from "./routes/account/upgrade.ts";
-import * as $6 from "./routes/api/items/[id].ts";
-import * as $7 from "./routes/api/items/index.ts";
-import * as $8 from "./routes/api/me/votes.ts";
-import * as $9 from "./routes/api/stripe-webhooks.ts";
-import * as $10 from "./routes/api/users/[login]/index.ts";
-import * as $11 from "./routes/api/users/[login]/items.ts";
-import * as $12 from "./routes/api/users/index.ts";
-import * as $13 from "./routes/api/vote.ts";
-import * as $14 from "./routes/blog/[slug].tsx";
-import * as $15 from "./routes/blog/index.tsx";
-import * as $16 from "./routes/dashboard/index.tsx";
-import * as $17 from "./routes/dashboard/stats.tsx";
-import * as $18 from "./routes/dashboard/users.tsx";
-import * as $19 from "./routes/feed.ts";
-import * as $20 from "./routes/index.tsx";
-import * as $21 from "./routes/pricing.tsx";
-import * as $22 from "./routes/submit.tsx";
-import * as $23 from "./routes/users/[login].tsx";
-import * as $24 from "./routes/welcome.tsx";
-import * as $$0 from "./islands/Chart.tsx";
-import * as $$1 from "./islands/ItemsList.tsx";
-import * as $$2 from "./islands/UsersTable.tsx";
+import * as $_404 from "./routes/_404.tsx";
+import * as $_500 from "./routes/_500.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $account_index from "./routes/account/index.tsx";
+import * as $account_manage from "./routes/account/manage.ts";
+import * as $account_upgrade from "./routes/account/upgrade.ts";
+import * as $api_items_id_ from "./routes/api/items/[id].ts";
+import * as $api_items_index from "./routes/api/items/index.ts";
+import * as $api_me_votes from "./routes/api/me/votes.ts";
+import * as $api_stripe_webhooks from "./routes/api/stripe-webhooks.ts";
+import * as $api_users_login_index from "./routes/api/users/[login]/index.ts";
+import * as $api_users_login_items from "./routes/api/users/[login]/items.ts";
+import * as $api_users_index from "./routes/api/users/index.ts";
+import * as $api_vote from "./routes/api/vote.ts";
+import * as $blog_slug_ from "./routes/blog/[slug].tsx";
+import * as $blog_index from "./routes/blog/index.tsx";
+import * as $dashboard_index from "./routes/dashboard/index.tsx";
+import * as $dashboard_stats from "./routes/dashboard/stats.tsx";
+import * as $dashboard_users from "./routes/dashboard/users.tsx";
+import * as $feed from "./routes/feed.ts";
+import * as $index from "./routes/index.tsx";
+import * as $pricing from "./routes/pricing.tsx";
+import * as $submit from "./routes/submit.tsx";
+import * as $users_login_ from "./routes/users/[login].tsx";
+import * as $welcome from "./routes/welcome.tsx";
+import * as $Chart from "./islands/Chart.tsx";
+import * as $ItemsList from "./islands/ItemsList.tsx";
+import * as $UsersTable from "./islands/UsersTable.tsx";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $0,
-    "./routes/_500.tsx": $1,
-    "./routes/_app.tsx": $2,
-    "./routes/account/index.tsx": $3,
-    "./routes/account/manage.ts": $4,
-    "./routes/account/upgrade.ts": $5,
-    "./routes/api/items/[id].ts": $6,
-    "./routes/api/items/index.ts": $7,
-    "./routes/api/me/votes.ts": $8,
-    "./routes/api/stripe-webhooks.ts": $9,
-    "./routes/api/users/[login]/index.ts": $10,
-    "./routes/api/users/[login]/items.ts": $11,
-    "./routes/api/users/index.ts": $12,
-    "./routes/api/vote.ts": $13,
-    "./routes/blog/[slug].tsx": $14,
-    "./routes/blog/index.tsx": $15,
-    "./routes/dashboard/index.tsx": $16,
-    "./routes/dashboard/stats.tsx": $17,
-    "./routes/dashboard/users.tsx": $18,
-    "./routes/feed.ts": $19,
-    "./routes/index.tsx": $20,
-    "./routes/pricing.tsx": $21,
-    "./routes/submit.tsx": $22,
-    "./routes/users/[login].tsx": $23,
-    "./routes/welcome.tsx": $24,
+    "./routes/_404.tsx": $_404,
+    "./routes/_500.tsx": $_500,
+    "./routes/_app.tsx": $_app,
+    "./routes/account/index.tsx": $account_index,
+    "./routes/account/manage.ts": $account_manage,
+    "./routes/account/upgrade.ts": $account_upgrade,
+    "./routes/api/items/[id].ts": $api_items_id_,
+    "./routes/api/items/index.ts": $api_items_index,
+    "./routes/api/me/votes.ts": $api_me_votes,
+    "./routes/api/stripe-webhooks.ts": $api_stripe_webhooks,
+    "./routes/api/users/[login]/index.ts": $api_users_login_index,
+    "./routes/api/users/[login]/items.ts": $api_users_login_items,
+    "./routes/api/users/index.ts": $api_users_index,
+    "./routes/api/vote.ts": $api_vote,
+    "./routes/blog/[slug].tsx": $blog_slug_,
+    "./routes/blog/index.tsx": $blog_index,
+    "./routes/dashboard/index.tsx": $dashboard_index,
+    "./routes/dashboard/stats.tsx": $dashboard_stats,
+    "./routes/dashboard/users.tsx": $dashboard_users,
+    "./routes/feed.ts": $feed,
+    "./routes/index.tsx": $index,
+    "./routes/pricing.tsx": $pricing,
+    "./routes/submit.tsx": $submit,
+    "./routes/users/[login].tsx": $users_login_,
+    "./routes/welcome.tsx": $welcome,
   },
   islands: {
-    "./islands/Chart.tsx": $$0,
-    "./islands/ItemsList.tsx": $$1,
-    "./islands/UsersTable.tsx": $$2,
+    "./islands/Chart.tsx": $Chart,
+    "./islands/ItemsList.tsx": $ItemsList,
+    "./islands/UsersTable.tsx": $UsersTable,
   },
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;

islands/ItemsList.tsx
@@ -2,7 +2,6 @@
 import { Signal, useComputed, useSignal } from "@preact/signals";
 import { useEffect } from "preact/hooks";
 import { type Item } from "@/utils/db.ts";
-import { LINK_STYLES } from "@/utils/constants.ts";
 import IconInfo from "tabler_icons_tsx/info-circle.tsx";
 import { fetchValues } from "@/utils/http.ts";
 import { decodeTime } from "std/ulid/mod.ts";
@@ -91,7 +90,7 @@ function ItemSummary(props: ItemSummaryProps) {
       <div class="space-y-1">
         <p>
           <a
-            class="visited:(text-[purple] dark:text-[lightpink]) hover:underline mr-4"
+            class="visited:text-[purple] visited:dark:text-[lightpink] hover:underline mr-4"
             href={props.item.url}
           >
             {props.item.title}
@@ -167,7 +166,7 @@ export default function ItemsList(props: ItemsListProps) {
   }, []);
 
   if (isLoadingSig.value === undefined) {
-    return <p class={LINK_STYLES}>Loading...</p>;
+    return <p class="link-styles">Loading...</p>;
   }
 
   return (
@@ -185,7 +184,7 @@ export default function ItemsList(props: ItemsListProps) {
         })
         : <EmptyItemsList />}
       {cursorSig.value !== "" && (
-        <button onClick={loadMoreItems} class={LINK_STYLES}>
+        <button onClick={loadMoreItems} class="link-styles">
           {isLoadingSig.value ? "Loading..." : "Load more"}
         </button>
       )}

islands/UsersTable.tsx
@@ -3,7 +3,6 @@ import { useSignal } from "@preact/signals";
 import { useEffect } from "preact/hooks";
 import type { User } from "@/utils/db.ts";
 import GitHubAvatarImg from "@/components/GitHubAvatarImg.tsx";
-import { LINK_STYLES } from "@/utils/constants.ts";
 import { fetchValues } from "@/utils/http.ts";
 import { PremiumBadge } from "@/components/PremiumBadge.tsx";
 
@@ -12,7 +11,7 @@ const TD_STYLES = "p-4";
 
 function UserTableRow(props: User) {
   return (
-    <tr class="hover:(bg-gray-50 dark:bg-gray-900) border-b border-gray-200">
+    <tr class="hover:bg-gray-50 hover:dark:bg-gray-900 border-b border-gray-200">
       <td scope="col" class={TD_STYLES}>
         <GitHubAvatarImg login={props.login} size={32} />
         <a
@@ -86,7 +85,7 @@ export default function UsersTable(props: UsersTableProps) {
       {cursorSig.value !== "" && (
         <button
           onClick={loadMoreUsers}
-          class={LINK_STYLES + " p-4"}
+          class="link-styles p-4"
         >
           {isLoadingSig.value ? "Loading..." : "Load more"}
         </button>

denoland/saaskit

commit 1d2e61c96f02a8fb378882450916a1c5c1ba2a88

fix: hamburger menu styling on smaller screens (#671)
"Someone" broke the hamburger menu in #646.

This fixes the issue and cleans things up by making the CSS a bit more
direct:
* I use
https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-sibling-state
to modify the `nav` directly.
* I add IDs to the hamburger and X components, and then use
https://tailwindcss.com/docs/hover-focus-and-other-states#using-arbitrary-variants
to directly target them, in combination with peer.

FYI @hashrock since this restores the great work you did in #374.

components/Header.tsx
@@ -21,10 +21,10 @@ export default function Header(props: HeaderProps) {
       <input
         type="checkbox"
         id="nav-toggle"
-        class="hidden [:checked&+*>:last-child>*>:first-child]:hidden [:checked&+*>:last-child>*>:last-child]:block checked:siblings:last-child:flex"
+        class="hidden peer"
       />
 
-      <div class="flex justify-between items-center">
+      <div class="flex justify-between items-center peer-checked:[&_>div>label>#IconMenu]:hidden peer-checked:[&_>div>label>#IconX]:block">
         <a href="/" class="shrink-0">
           <img
             height="48"
@@ -41,8 +41,8 @@ export default function Header(props: HeaderProps) {
             id="nav-toggle-label"
             htmlFor="nav-toggle"
           >
-            <IconMenu class="size-6" />
-            <IconX class="hidden size-6" />
+            <IconMenu class="size-6" id="IconMenu" />
+            <IconX class="hidden size-6" id="IconX" />
           </label>
         </div>
       </div>
@@ -58,7 +58,7 @@ export default function Header(props: HeaderProps) {
         `}
       </script>
       <nav
-        class={"hidden flex-col gap-x-4 divide-y divide-solid sm:flex sm:items-center sm:flex-row sm:divide-y-0"}
+        class={"hidden flex-col gap-x-4 divide-y divide-solid sm:flex sm:items-center sm:flex-row sm:divide-y-0 peer-checked:flex"}
       >
         <a
           href="/dashboard"