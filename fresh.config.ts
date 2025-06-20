bd3dd232d34f2539faf873b7aa2c0ce4900beaeb
Asher Gomez <ashersaupingomez@gmail.com>
2025-06-20 13:57:12 +1000

    refactor: move welcome plugin into middleware

--- fresh.config.ts
+++ fresh.config.ts
@@ -4,7 +4,6 @@ import kvOAuthPlugin from "./plugins/kv_oauth.ts";
 import sessionPlugin from "./plugins/session.ts";
 import errorHandling from "./plugins/error_handling.ts";
 import securityHeaders from "./plugins/security_headers.ts";
-import welcomePlugin from "./plugins/welcome.ts";
 import type { FreshConfig } from "$fresh/server.ts";
 import { ga4Plugin } from "https://deno.land/x/fresh_ga4@0.0.4/mod.ts";
 import { blog } from "./plugins/blog/mod.ts";
@@ -12,7 +11,6 @@ import { blog } from "./plugins/blog/mod.ts";
 export default {
   plugins: [
     ga4Plugin(),
-    welcomePlugin,
     kvOAuthPlugin,
     sessionPlugin,
     tailwind(),
